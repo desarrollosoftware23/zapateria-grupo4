b4d480db96c60c7be2bcc1913bf7288eb3820900
jostynrdgz-gif <jostynrodriguez466@gmail.com>
2026-06-22 09:12:36 -0500

    se agrego campos automaticos para calcular descuento

---
+++ zapato_descuento/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'zapatos',
+    'version': '1.0.1',
+    'author':'Jostyn Rodriguez',
+    'category': 'sales',
+    'depends': ['zapatos', 'zapatos_extensionprofe'],
+    'data': [
+        'views/zapato_descuento_views.xml',
+    ],
+     'installable': True,
+    'application': True,
+}
